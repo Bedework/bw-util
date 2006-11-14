4dadb6d958b595054996dc3524f0a2a48fc66c48
Mike Douglass <douglm@rpi.edu>
2006-11-14 16:57:43 +0000

    Upgrade to lucene 2.0.0
    Add limits to searches - ui changes still pending.

--- src/edu/rpi/cct/misc/indexing/IndexLuceneImpl.java
+++ src/edu/rpi/cct/misc/indexing/IndexLuceneImpl.java
@@ -1,3 +1,28 @@
+/* **********************************************************************
+    Copyright 2006 Rensselaer Polytechnic Institute. All worldwide rights reserved.
+
+    Redistribution and use of this distribution in source and binary forms,
+    with or without modification, are permitted provided that:
+       The above copyright notice and this permission notice appear in all
+        copies and supporting documentation;
+
+        The name, identifiers, and trademarks of Rensselaer Polytechnic
+        Institute are not used in advertising or publicity without the
+        express prior written permission of Rensselaer Polytechnic Institute;
+
+    DISCLAIMER: The software is distributed" AS IS" without any express or
+    implied warranty, including but not limited to, any implied warranties
+    of merchantability or fitness for a particular purpose or any warrant)'
+    of non-infringement of any current or pending patent rights. The authors
+    of the software make no representations about the suitability of this
+    software for any particular purpose. The entire risk as to the quality
+    and performance of the software is with the user. Should the software
+    prove defective, the user assumes the cost of all necessary servicing,
+    repair or correction. In particular, neither Rensselaer Polytechnic
+    Institute, nor the authors of the software are liable for any indirect,
+    special, consequential, or incidental damages related to the software,
+    to the maximum extent the law permits.
+*/
 package edu.rpi.cct.misc.indexing;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -11,6 +36,7 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.Filter;
+import org.apache.lucene.search.FilteredQuery;
 import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
@@ -437,7 +463,7 @@ public abstract class IndexLuceneImpl implements Index {
    * which is determined by the underlying implementation.
    *
    * @param   query    Query string
-   * @param   filter   Filter tro apply or null
+   * @param   filter   Filter to apply or null
    * @return  int      Number found. 0 means none found,
    *                                -1 means indeterminate
    * @throws IndexException
@@ -452,6 +478,10 @@ public abstract class IndexLuceneImpl implements Index {
       }
 
       Query parsed = queryParser.parse(query);
+
+      if (filter != null) {
+        parsed = new FilteredQuery(parsed, filter);
+      }
 
       if (debug) {
         trace("     with parsed query " + parsed.toString(null));
