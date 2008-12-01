da875e20c65312e88c3221ad43418efdee4cf806
Mike Douglass <douglm@rpi.edu>
2008-12-01 02:13:54 +0000

    Almost null change

--- src/edu/rpi/sss/util/Util.java
+++ src/edu/rpi/sss/util/Util.java
@@ -23,7 +23,6 @@
     special, consequential, or incidental damages related to the software,
     to the maximum extent the law permits.
 */
-
 package edu.rpi.sss.util;
 
 import java.io.InputStream;
