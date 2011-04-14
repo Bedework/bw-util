b0be8c8554103c72ffe841285346d9a1b59138b0
Mike Douglass <douglm@rpi.edu>
2011-04-14 15:22:05 +0000

    More changes to SOAP service. About to significantly change update approach

--- src/edu/rpi/sss/util/xml/tagdefs/CalWSXrdDefs.java
+++ src/edu/rpi/sss/util/xml/tagdefs/CalWSXrdDefs.java
@@ -6,9 +6,9 @@
     Version 2.0 (the "License"); you may not use this file
     except in compliance with the License. You may obtain a
     copy of the License at:
-        
+
     http://www.apache.org/licenses/LICENSE-2.0
-        
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the License is distributed on
     an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -113,4 +113,8 @@ public class CalWSXrdDefs {
 
   /** Link property */
   public static final String timezone = namespace + "/" + "timezone";
+
+  /** Link property */
+  public static final String supportedCalendarComponentSet =
+    namespace + "/" + "supported-calendar-component-set";
 }
