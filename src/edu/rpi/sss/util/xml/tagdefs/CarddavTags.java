d4e8986456f508ce7f8c1778c6efc4edbe516c1d
Mike Douglass <douglm@rpi.edu>
2013-01-14 20:23:24 +0000

    Add directory element for carddav gateway support

--- src/edu/rpi/sss/util/xml/tagdefs/CarddavTags.java
+++ src/edu/rpi/sss/util/xml/tagdefs/CarddavTags.java
@@ -6,9 +6,9 @@
     Version 2.0 (the "License"); you may not use this file
     except in compliance with the License. You may obtain a
     copy of the License at:
-        
+
     http://www.apache.org/licenses/LICENSE-2.0
-        
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the License is distributed on
     an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -62,6 +62,9 @@ public class CarddavTags {
 
   /** */
   public static final QName allprop = makeQName("allprop");
+
+  /**   */
+  public static final QName directory = makeQName("directory");
 
   /**   */
   public static final QName filter = makeQName("filter");
