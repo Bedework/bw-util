0289887b0d541cfb9ae987b1de67483df17e8b0c
mike <mikeadouglass@gmail.com>
2024-10-21 22:51:57 -0400

    Fix up toString for access. Add a bunch of features to the ToString class

--- bw-util-misc/src/main/java/org/bedework/util/misc/ToString.java
+++ bw-util-misc/src/main/java/org/bedework/util/misc/ToString.java
@@ -28,15 +28,27 @@ import java.util.Collection;
  */
 public class ToString {
   private StringBuilder sb;
-  private String indent = "";
+  private int indents;
   private String delim = "";
 
   private int lastNewLinePos = 0;
 
   private boolean valuesOnly;
+  private boolean delimitersOn = true;
 
   private final static int maxLen = 80;
   private final static String indentVal = "  ";
+
+  private int lineCt;
+  private int objStartLineCt;
+
+  /** Objects implementing this can be called with the
+   * current ToString object.
+   *
+   */
+  public interface ToStringProducer {
+    void toStringWith(ToString ts);
+  }
 
   /** Create an instance for the given object
    *
@@ -44,6 +56,7 @@ public class ToString {
    */
   public ToString(final Object o) {
     sb = new StringBuilder(o.getClass().getSimpleName()).append("{");
+    indents++;
   }
 
   private ToString() {
@@ -64,12 +77,21 @@ public class ToString {
   /** Create an instance for the given object and indentation
    *
    * @param o the object
-   * @param indent the indent
+   * @param indents number of indentVal spaces
    */
-  public ToString(final Object o, final String indent) {
-    sb = new StringBuilder(indent);
+  public ToString(final Object o, final int indents) {
+    sb = new StringBuilder();
+    sb.append(indentVal.repeat(Math.max(0, indents)));
     sb.append(o.getClass().getSimpleName()).append("{");
-    this.indent = indent;
+    this.indents = Math.max(0, indents);
+  }
+
+  public ToString initClass(final Object o) {
+    sb.append(o.getClass().getSimpleName()).append("{");
+    clearDelim();
+    indents++;
+    objStartLineCt = lineCt;
+    return this;
   }
 
   /**
@@ -79,15 +101,48 @@ public class ToString {
     return sb;
   }
 
+  public ToString delimitersOn() {
+    delimitersOn = true;
+
+    return this;
+  }
+
+  public ToString delimitersOff() {
+    delimitersOn = false;
+
+    return this;
+  }
+
+  public ToString clearDelim() {
+    delim = "";
+
+    return this;
+  }
+
   /** add list delimiter
    *
    * @return this
    */
   public ToString delimit() {
-    sb.append(delim);
-    delim = ", ";
+    if (delimitersOn) {
+      sb.append(delim);
+      delim = ", ";
+    }
     if (lineLength() > maxLen) {
       outputNewLine();
+    }
+
+    return this;
+  }
+
+  public ToString indentIn() {
+    indents++;
+    return this;
+  }
+
+  public ToString indentOut() {
+    if (indents > 0) {
+      indents--;
     }
 
     return this;
@@ -114,6 +169,31 @@ public class ToString {
   }
 
   /**
+   * @param value to append quoted
+   * @return this object
+   */
+  public ToString appendQ(final String value) {
+    delimit();
+    sb.append("\"")
+      .append(value)
+      .append("\"");
+
+    return this;
+  }
+
+  /**
+   * @param value to append in parentheses - no space or delim
+   * @return this object
+   */
+  public ToString appendParen(final String value) {
+    sb.append("(")
+      .append(value)
+      .append(")");
+
+    return this;
+  }
+
+  /**
    * @param value to append
    * @return this object
    */
@@ -129,8 +209,20 @@ public class ToString {
    * @return this object
    */
   public ToString append(final Object value) {
-    delimit();
-    sb.append(value);
+    return delimit()
+            .appendObject(value);
+  }
+
+  /**
+   * @param value to append
+   * @return this object
+   */
+  public ToString appendObject(final Object value) {
+    if (value instanceof ToStringProducer) {
+      ((ToStringProducer)value).toStringWith(this);
+    } else {
+      sb.append(value);
+    }
 
     return this;
   }
@@ -152,6 +244,7 @@ public class ToString {
       return this;
     }
 
+    indentIn();
     for (final Object o: val){
       if (withNewLines) {
         newLine();
@@ -160,6 +253,7 @@ public class ToString {
       append(o);
     }
 
+    indentOut().newLine();
     sb.append("]");
 
     return this;
@@ -170,11 +264,10 @@ public class ToString {
    * @param value an object
    * @return this object
    */
-  public ToString append(final String name, final Object value) {
-    nameEquals(name);
-    sb.append(value);
-
-    return this;
+  public ToString append(final String name,
+                         final Object value) {
+    return nameEquals(name)
+            .appendObject(value);
   }
 
   /**
@@ -182,7 +275,8 @@ public class ToString {
    * @param value Long
    * @return this object
    */
-  public ToString append(final String name, final Long value) {
+  public ToString append(final String name,
+                         final Long value) {
     nameEquals(name);
     sb.append(value);
 
@@ -208,8 +302,7 @@ public class ToString {
     delim= "";
 
     for (final Object o: value) {
-      delimit();
-      sb.append(o);
+      append(o);
     }
     sb.append("]");
 
@@ -223,7 +316,8 @@ public class ToString {
    * @param value boolean
    * @return this object
    */
-  public ToString append(final String name, final boolean value) {
+  public ToString append(final String name,
+                         final boolean value) {
     return append(name, String.valueOf(value));
   }
 
@@ -232,7 +326,8 @@ public class ToString {
    * @param value int
    * @return this object
    */
-  public ToString append(final String name, final int value) {
+  public ToString append(final String name,
+                         final int value) {
     return append(name, String.valueOf(value));
   }
 
@@ -244,25 +339,35 @@ public class ToString {
     return append("Exception", t.getMessage());
   }
 
-  @Override
-  public String toString() {
+  public ToString nameEquals(final String name) {
+    delimit();
+    sb.append(name);
+    sb.append("=");
+
+    return this;
+  }
+
+  public ToString closeClass() {
+    indentOut();
+    if (objStartLineCt != lineCt) {
+      outputNewLine();
+    }
     if (!valuesOnly) {
       sb.append("}");
     }
 
-    return sb.toString();
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return closeClass().getSb().toString();
   }
 
   private void outputNewLine() {
     sb.append("\n");
     lastNewLinePos = sb.length();
-    sb.append(indent);
-    sb.append(indentVal);
-  }
-
-  private void nameEquals(final String name) {
-    delimit();
-    sb.append(name);
-    sb.append("=");
+    lineCt++;
+    sb.append(indentVal.repeat(indents));
   }
 }
